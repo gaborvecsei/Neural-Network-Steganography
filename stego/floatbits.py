2cca718f53ddc3771d5aeb05cb3b6922f700cffa
Gabor Vecsei <vecseigabor.x@gmail.com>
2022-02-22 18:15:38 +0100

    comments added

--- stego/floatbits.py
+++ stego/floatbits.py
@@ -7,7 +7,10 @@ class FloatBinary:
     def __init__(self, v: float) -> None:
         self.v: float = float(v)
 
+        # Binary representation of the float value LSB
         self._bin: Tuple[bool] = self.float_to_binary(self.v)
+
+        # Binary representation splitted to sign, exponent, fraction/mantissa
         self._splitted_bin: dict = self.split_binary(self._bin)
 
         # 1 bit for sign
@@ -19,25 +22,37 @@ class FloatBinary:
         # Full 23 bits for fraction/mantissa - 3 parts: first 7 bits, middle 8 bits, last 8 bits
         self.fraction: Tuple[bool] = self._splitted_bin["fraction"]
 
-    def get_fraction_part(self, part: int) -> Tuple[bool]:
-        if part == 0:
-            ret_values = self.fraction[:8]
-        elif part == 1:
-            ret_values = self.fraction[8:17]
-        elif part == 2:
-            ret_values = self.fraction[17:]
-        else:
-            ValueError("Only parts 0, 1, 2 are acceptable")
-
-        return ret_values
-
     def float_to_binary(self, num: float) -> Tuple[bool]:
+        """
+        Convert float value to a binary representation - 1s and 0s
+        (True is 1 and False is 0)
+
+        Args:
+            num: float value
+
+        Returns:
+            Tuple[bool]: tuple of bools
+        """
+
         bin_num = bin(ctypes.c_uint.from_buffer(ctypes.c_float(num)).value)[2:]
         bin_num = bin_num.rjust(32, "0")
         return tuple([False if x == "0" else True for x in list(bin_num)])
 
     @staticmethod
     def split_binary(binary_float: Tuple[bool]) -> dict:
+        """
+        Structures (splits) the binary representation to sign, exponent, fraction
+
+        Args:
+            binary_float: binary representation of the float value
+
+        Returns:
+            dict: part name and binary representation
+        """
+
+        if len(binary_float) != 32:
+            raise ValueError("You need 32 bits")
+
         sign = binary_float[0]
         exponent = binary_float[1:9]
         fraction = binary_float[9:32]
@@ -45,7 +60,26 @@ class FloatBinary:
         return d
 
     @staticmethod
-    def _reconstruct_float_value(sign, exponent, fraction) -> float:
+    def _reconstruct_float_value(sign: bool, exponent: Tuple[bool], fraction: Tuple[bool]) -> float:
+        """
+        From the binary float representation "reconstruct" (calculate) the float value
+
+        Args:
+            sign: sign value as a bool
+            exponent: exponent as a tuple of bools
+            fraction: fraction as a tuple of bools
+
+        Returns:
+            float: calculated float value
+        """
+
+        if len(fraction) != 23:
+            raise ValueError("Fraction should be 23 values bits")
+        if len(exponent) != 8:
+            raise ValueError("Exponent should be 8 bits")
+        if isinstance(sign, bool):
+            raise ValueError("Sign is a single bit")
+
         fraction = (True,) + fraction
         fraction_as_int = int(FloatBinary.bool_to_str_representation(fraction), 2) / 2**23
         exponent_as_int = int(FloatBinary.bool_to_str_representation(exponent), 2) - 127
@@ -53,6 +87,13 @@ class FloatBinary:
         return sign_as_int * fraction_as_int * (2**exponent_as_int)
 
     def reconstruct_float_value(self) -> float:
+        """
+        From the binary float representation "reconstruct" (calculate) the float value
+
+        Returns:
+            float: calculated float value
+        """
+
         return self._reconstruct_float_value(self.sign, self.exponent, self.fraction)
 
     @staticmethod
@@ -73,4 +114,3 @@ class FloatBinary:
 
     def as_bit_str(self) -> str:
         return self.bool_to_str_representation(self._bin)
-
